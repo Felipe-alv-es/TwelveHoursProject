b60d7a42d17f128ab384f7604877620cad2f81d2
Felipe-alv-es <brago.sama@gmail.com>
2024-03-15 18:46:58 -0300

    Created the svg for addButton

--- src/components/TaskGroup/TaskGroup.tsx
+++ src/components/TaskGroup/TaskGroup.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box, Button } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import { StyledTypography, StyledContainer } from "./TaskGroup.styles.tsx";
 import TaskItem from "../TaskItem/TaskItem.tsx";
 import { TaskGroupProps } from "./TaskGroup.types.ts";
@@ -73,9 +73,86 @@ const TaskGroup = React.forwardRef<HTMLDivElement, TaskGroupProps>(
           </Box>
           <Box>
             {renderItems()}
-            <Button variant="contained" onClick={() => handleAddItem()}>
-              Clica em Mim
-            </Button>
+            <Box
+              sx={{
+                height: "fit-content",
+                alignSelf: "end",
+                marginRight: "2%",
+                "> button": {
+                  "> svg": {
+                    width: "50px",
+                    height: "50px",
+                  },
+                },
+              }}
+            >
+              <IconButton
+                sx={{ alignItems: "center" }}
+                onClick={() => handleAddItem()}
+              >
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="200px"
+                  width="200px"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <defs>
+                    <linearGradient
+                      id="myGradient"
+                      x1="0%"
+                      y1="0%"
+                      x2="100%"
+                      y2="0%"
+                    >
+                      <stop offset="0%" stop-color="#B34684" />
+                      <stop offset="100%" stop-color="#B3A446" />
+                    </linearGradient>
+                  </defs>
+                  <circle cx="12" cy="12" r="10" stroke="url(#myGradient)" />
+                  <path d="M8 12h8" stroke="currenColor" />
+                  <path d="M12 8v8" stroke="currenColor" />
+                </svg>
+                <Box
+                  sx={{
+                    width: "35px",
+                    height: "35px",
+                    display: "flex",
+                    position: "absolute",
+                  }}
+                >
+                  <svg
+                    viewBox="0 0 448 512"
+                    height="100%"
+                    width="200px"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <defs>
+                      <linearGradient
+                        id="plusGradient"
+                        x1="0%"
+                        y1="0%"
+                        x2="100%"
+                        y2="0%"
+                      >
+                        <stop offset="0%" stopColor="#B34684" />
+                        <stop offset="100%" stopColor="#B3A446" />
+                      </linearGradient>
+                    </defs>
+                    <path
+                      d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
+                      stroke="currentColor"
+                      fill="url(#plusGradient)"
+                      strokeWidth="0"
+                    />
+                  </svg>
+                </Box>
+              </IconButton>
+            </Box>
           </Box>
         </StyledContainer>
       </Box>
