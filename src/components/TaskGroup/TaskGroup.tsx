d439a0e2e30cd703254bbb249e6ea7502de38514
Felipe-alv-es <brago.sama@gmail.com>
2024-03-15 20:45:39 -0300

    Separated the addIcon in a new component

--- src/components/TaskGroup/TaskGroup.tsx
+++ src/components/TaskGroup/TaskGroup.tsx
@@ -3,6 +3,7 @@ import { Box, IconButton } from "@mui/material";
 import { StyledTypography, StyledContainer } from "./TaskGroup.styles.tsx";
 import TaskItem from "../TaskItem/TaskItem.tsx";
 import { TaskGroupProps } from "./TaskGroup.types.ts";
+import AddButton from "../AddButton/AddButton.tsx";
 
 const TaskGroup = React.forwardRef<HTMLDivElement, TaskGroupProps>(
   ({ role, quantity = 0 }, ref) => {
@@ -72,87 +73,16 @@ const TaskGroup = React.forwardRef<HTMLDivElement, TaskGroupProps>(
             </StyledTypography>
           </Box>
           <Box>
-            {renderItems()}
-            <Box
+            {renderItems()}{" "}
+            <IconButton
               sx={{
+                alignItems: "center",
                 height: "fit-content",
-                alignSelf: "end",
-                marginRight: "2%",
-                "> button": {
-                  "> svg": {
-                    width: "50px",
-                    height: "50px",
-                  },
-                },
               }}
+              onClick={() => handleAddItem()}
             >
-              <IconButton
-                sx={{ alignItems: "center" }}
-                onClick={() => handleAddItem()}
-              >
-                <svg
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="200px"
-                  width="200px"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <defs>
-                    <linearGradient
-                      id="myGradient"
-                      x1="0%"
-                      y1="0%"
-                      x2="100%"
-                      y2="0%"
-                    >
-                      <stop offset="0%" stop-color="#B34684" />
-                      <stop offset="100%" stop-color="#B3A446" />
-                    </linearGradient>
-                  </defs>
-                  <circle cx="12" cy="12" r="10" stroke="url(#myGradient)" />
-                  <path d="M8 12h8" stroke="currenColor" />
-                  <path d="M12 8v8" stroke="currenColor" />
-                </svg>
-                <Box
-                  sx={{
-                    width: "35px",
-                    height: "35px",
-                    display: "flex",
-                    position: "absolute",
-                  }}
-                >
-                  <svg
-                    viewBox="0 0 448 512"
-                    height="100%"
-                    width="200px"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <defs>
-                      <linearGradient
-                        id="plusGradient"
-                        x1="0%"
-                        y1="0%"
-                        x2="100%"
-                        y2="0%"
-                      >
-                        <stop offset="0%" stopColor="#B34684" />
-                        <stop offset="100%" stopColor="#B3A446" />
-                      </linearGradient>
-                    </defs>
-                    <path
-                      d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-                      stroke="currentColor"
-                      fill="url(#plusGradient)"
-                      strokeWidth="0"
-                    />
-                  </svg>
-                </Box>
-              </IconButton>
-            </Box>
+              <AddButton />
+            </IconButton>
           </Box>
         </StyledContainer>
       </Box>
