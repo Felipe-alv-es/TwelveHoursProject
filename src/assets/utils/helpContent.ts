80f023538a10006651c845c441c34772262724b8
Felipe-alv-es <brago.sama@gmail.com>
2024-03-22 16:33:46 -0300

    finished help modal

---
+++ src/assets/utils/helpContent.ts
@@ -0,0 +1,22 @@
+export const pagesContent = [
+  {
+    id: 1,
+    title: "O que é a Tecnica das 12 horas?",
+    title2: "Como organizar seu tempo?",
+    title3: "Renda Extra",
+    title4: "Network",
+    title5: "Requalificação Profissional",
+    text1:
+      "\n Reserve 5 horas para atividades que possam gerar uma renda extra. É crucial considerar que alcançar uma renda adicional significativa em um curto espaço de tempo pode ser extremamente desafiador, especialmente quando se está desempregado. Portanto, dedique esse tempo a atividades remuneradas, como trabalhar como entregador, vender produtos, prestar serviços como motorista de aplicativo, entre outras opções. \n\n",
+    text2:
+      "\nUm dos maiores equívocos das pessoas é dedicar seu tempo exclusivamente a uma única tarefa, negligenciando outras prioridades, até que se vejam sem recursos e em desespero. Essa técnica visa corrigir essa abordagem.\n \nA técnica das 12 horas consiste em dividir seu tempo de forma desigual, dando mais atenção às áreas que demandam mais esforço, como a geração de uma renda extra, sem, no entanto, negligenciar outras prioridades, como o networking e a requalificação.\n\nVisando aprimorar o gasto do tempo preenchendo todos os requisitos, faremos uma distribuição de tempo da seguinte forma:\n\n",
+    text3:
+      "\nPara renda extra será dedicado o tempo de 5 horas, visto que fazer uma renda extra consideravel em pouco tempo é extremamente ineficiente, além de quando se esta desempregado, tornasse imprecindivel. \nSendo assim, dedique essas horas para atividades remuneradas, seja trabalhando como entregador, vendendo produtos como bolo de pote, motorista de uber, entre outros.\n\n",
+    text4:
+      "\nUtilize parte do seu tempo para estabelecer novas conexões com profissionais da sua área, seja por meio do LinkedIn, Instagram ou outras redes relevantes. Durante esse período, destaque aspectos importantes do seu desenvolvimento profissional, como estudos em andamento, projetos nos quais está envolvido, pessoas influentes em sua rede, participações em eventos relevantes, entre outros. Evite compartilhar conteúdos irrelevantes e foque em construir conexões valiosas que possam impulsionar sua carreira.\n\n",
+    text5:
+      "\nDedique uma parcela do seu tempo para atividades que possam melhorar sua qualificação e promover seu crescimento no mercado de trabalho. Isso pode incluir a realização de cursos, especializações, o desenvolvimento de projetos relevantes e outras iniciativas que agreguem valor à sua trajetória profissional, facilitando sua recolocação e destacando você como um candidato diferenciado em relação aos demais concorrentes.\n",
+    text6:
+      "\nAo seguir essa abordagem organizada, você poderá otimizar seu tempo, concentrando-se nos pontos essenciais e importantes para sua recolocação profissional. Desta forma, estará mais preparado para aproveitar as oportunidades de emprego que surgirem no futuro próximo.\n\n",
+  },
+];
