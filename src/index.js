5d6564c1ddd35abe920a71971c0ea609816cbbe2
Felipe-alv-es <brago.sama@gmail.com>
2024-04-16 11:57:13 -0300

    fixed timer counter when tab focus changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyles from "./assets/styles/global";
+// eslint-disable-next-line no-unused-vars
+import HackTimer from "./assets/scripts/HackTimer/HackTimer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
